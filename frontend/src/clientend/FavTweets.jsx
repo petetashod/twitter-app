2ce832979a261d3db5eae0a449099c0091aa6dee
tashod peterson <tashodpeterson@tashods-MacBook-Air.local>
2023-11-02 21:58:58 -0400

    first

--- frontend/src/clientend/FavTweets.jsx
+++ frontend/src/clientend/FavTweets.jsx
@@ -5,52 +5,22 @@ import "/src/App.css";
 import axios from "axios";
 
 function FavTweets() {
-  // let twitterData = {
-  //   name: "Luke Skywalker",
-  //   height: "172",
-  //   mass: "77",
-  //   hair_color: "blond",
-  //   skin_color: "fair",
-  //   eye_color: "blue",
-  //   birth_year: "19BBY",
-  //   gender: "male",
-  //   homeworld: "https://swapi.dev/api/planets/1/",
-  //   films: [
-  //     "https://swapi.dev/api/films/2/",
-  //     "https://swapi.dev/api/films/6/",
-  //     "https://swapi.dev/api/films/3/",
-  //     "https://swapi.dev/api/films/1/",
-  //     "https://swapi.dev/api/films/7/",
-  //   ],
-  //   species: ["https://swapi.dev/api/species/1/"],
-  //   vehicles: [
-  //     "https://swapi.dev/api/vehicles/14/",
-  //     "https://swapi.dev/api/vehicles/30/",
-  //   ],
-  //   starships: [
-  //     "https://swapi.dev/api/starships/12/",
-  //     "https://swapi.dev/api/starships/22/",
-  //   ],
-  //   created: "2014-12-09T13:50:51.644000Z",
-  //   edited: "2014-12-20T21:17:56.891000Z",
-  //   url: "https://swapi.dev/api/people/1/",
-  // };
 
   const [tweetsDisplay, setTweetsDisplay] = useState([]);
-
-  const [url, setUrl] = useState("http://localhost:3000/api/tweets");
-  useEffect(() => {
-    let favTweetInfo = async () => {
-      let response = await axios.get(url);
-      let tweetInformation = response.data.data;
-      console.log(tweetInformation);
-      setTweetsDisplay(tweetInformation);
-    };
-    favTweetInfo();
-  }, [url]);
+  
+  const url = "http://localhost:3000/api/tweets"
+  
 
   const button = (e) => {
     e.preventDefault();
+    let favTweetInfo = async () => {
+      let response = await axios.get(url);
+      let tweetInformation = response.data.data;
+      // console.log(tweetInformation);
+      setTweetsDisplay(tweetInformation);
+      
+    };
+    favTweetInfo();
   };
   // const tweetsDisplayArray = [...tweetsDisplay];
 
@@ -73,11 +43,11 @@ function FavTweets() {
           </button>
         </form>
       </div>
-      <div>
+      <div className="card-container">
         {tweetsDisplay.map((tweet, index) => (
-          <ul key={index}>
-            <li>{tweet[0]}</li>
-            <li>{tweet[1]}</li>
+          <ul className="card" key={index}>
+            <h5 className="card-title">{tweet.id}</h5>
+            <p className="card-text">{tweet.text}</p>
           </ul>
         ))}
       </div>
